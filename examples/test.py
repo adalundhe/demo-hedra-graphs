75af455c01976423b0438bc34d7a4cd5ce3c1a26
scorbettUM <sean.corbett@umontana.edu>
2022-12-08 14:03:55 -0600

    Hedra graph update: /home/sean/Documents/Demos/hedra_cloud - 2022-12-08T14:03:55Z

---
+++ examples/test.py
@@ -0,0 +1,31 @@
+from hedra import (
+	Setup,
+	Execute,
+	Analyze,
+	Submit,
+	JSONConfig,
+	action,
+)
+
+class SetupStage(Setup):
+    batch_size=1000
+    total_time='1m'
+
+
+class ExecuteStage(Execute):
+
+    @action()
+    async def http_get(self):
+        return await self.client.http.get('')
+
+
+class AnalyzeStage(Analyze):
+    pass
+
+
+class SubmitResultsStage(Submit):
+    config=JSONConfig(
+        events_filepath='./events.json',
+        metrics_filepath='./metrics.json'
+    )
+
